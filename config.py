598be3125d741defbbf9bbf385483fa671a91a36
Enes Ahmeti <sundley.12345@gmail.com>
2023-01-27 16:56:32 +0300

    added looping through pages

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 """Configuration file for all the code parameters."""
 
-URL = "https://www.veturaneshitje.com/vetura?search=&make=&model=&cartype=&pricefrom=&priceto=&yearfrom=&yearto=&enginesizefrom=&enginesizeto=&kilometersfrom=&kilometersto=&fueltype=&transmission=&color=&seat=&city=&ownership=&damage=on"
+URL = "https://www.veturaneshitje.com/vetura?page="
 PAGES_TO_SCRAPE = 2
 BASE_URL = "https://www.veturaneshitje.com"
 DATASET_FOLDER = "data"
